3101a6838e2f3bcf2547fa81a293a76b959f84bf
Amar Deep <amard0819@gmail.com>
2026-05-26 20:05:59 +0530

    Amar/Dsa

---
+++ findMissingNumber.js
@@ -0,0 +1,16 @@
+function findMissingNumber(arr) {
+
+    let n = arr.length + 1;
+
+    let expectedSum = (n * (n + 1)) / 2;
+
+    let actualSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        actualSum += arr[i];
+    }
+
+    return expectedSum - actualSum;
+}
+
+console.log(findMissingNumber([1, 2, 3, 5]));
